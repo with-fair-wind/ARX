8c3582a8bb63f64717ce73bb75730b5d271f2b6d
kk <yangyukai@zwcad.com>
2025-03-10 17:58:34 +0800

    update

--- main/src/test1.cpp
+++ main/src/test1.cpp
@@ -4,6 +4,9 @@ namespace Test
 {
     void test1()
     {
+        AcDbLine *pline = new AcDbLine(AcGePoint3d::kOrigin, {100, 100, 0});
+        KK::KKArxUtility utility;
+        utility.postToModelSpace(pline);
     }
 
 } // namespace Test
